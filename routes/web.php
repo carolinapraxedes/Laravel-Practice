7faf045ae8f45c8176cbeb4a044993163d817ba4
Carolina Praxedes <ana_carolina47@hotmail.com>
2022-08-19 16:27:23 -0300

    Finalização da auth

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,8 @@ Route::get('/',function(){
 });
 
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth'])->name('dashboard');
+ Route::get('/dashboard', function () {
+     return view('dashboard');
+ })->middleware(['auth'])->name('dashboard');
 
-// require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';
